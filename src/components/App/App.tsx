7d2fa0ae940517511947ba0ae27b4493c8e66e25
George-Sava <73170427+George-Sava@users.noreply.github.com>
2023-03-23 11:49:55 +0200

    Budget app (#7)
    * budget-app v1
    
    * updated styles
    
    ---------
    
    Co-authored-by: GeorgeAlexandruSava

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -19,6 +19,67 @@ export function App() {
 		<div className="flex-container">
 			<h2>Total budget is: {getTotalBudget(budget)}</h2>
 
+
+			<div className="inner-container">
+				<div>
+					<h3>Insert monthly income:</h3>
+					<input type="number" ref={incomeRef} />
+					<div className="btn-container">
+						<button
+							onClick={() => {
+								const income = incomeRef.current?.value;
+								if (income) {
+									incomeRef.current.value = '';
+									setBudget(prevBudget =>
+										addBudgetItem(prevBudget, {
+											name: 'Income',
+											amount: Number(income)
+										})
+									);
+								}
+							}}
+						>
+							Submit
+						</button>
+					</div>
+
+					<h3>Insert expense:</h3>
+					<div className="expense-container">
+						<label htmlFor="expense-name">Name:</label>
+						<input type="text" ref={expenseName} />
+					</div>
+					<div className="expense-container">
+						<label htmlFor="expense-amount">Amount:</label>
+						<input type="number" ref={expenseAmount} />
+					</div>
+					<div className="btn-container">
+						<button
+							onClick={() => {
+								const name = expenseName.current?.value;
+								const amount = expenseAmount.current?.value;
+								if (name && amount) {
+									expenseName.current.value = '';
+									expenseAmount.current.value = '';
+									setBudget(prevBudget =>
+										addBudgetItem(prevBudget, { name, amount: Number(amount) })
+									);
+								}
+							}}
+						>
+							Submit
+						</button>
+					</div>
+				</div>
+				<div>
+					<h3>Expenses items:</h3>
+					<ul>
+						{budgetItems.map((node, i) => (
+							<li key={`${node.item.name}-${i}`}>
+								{node.item.name}: {node.item.amount}
+							</li>
+						))}
+					</ul>
+				</div>
 			<div>
 				<h3>Insert monthly income:</h3>
 				<input type="number" ref={incomeRef} />
