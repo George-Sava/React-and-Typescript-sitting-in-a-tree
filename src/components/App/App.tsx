5608224c9a27d7803b208b9bdd69944660c2949d
GeorgeAlexandruSava <george@sensidev.net>
2023-03-23 12:07:43 +0200

    fixed html

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -79,57 +79,6 @@ export function App() {
 						))}
 					</ul>
 				</div>
-				<div>
-					<h3>Insert monthly income:</h3>
-					<input type="number" ref={incomeRef} />
-					<button
-						onClick={() => {
-							const income = incomeRef.current?.value;
-							if (income) {
-								incomeRef.current.value = '';
-								setBudget(prevBudget =>
-									addBudgetItem(prevBudget, {
-										name: 'Income',
-										amount: Number(income)
-									})
-								);
-							}
-						}}
-					>
-						Submit
-					</button>
-
-					<h3>Insert expense:</h3>
-					<label htmlFor="expense-name">Name:</label>
-					<input type="text" ref={expenseName} />
-					<label htmlFor="expense-amount">Amount:</label>
-					<input type="number" ref={expenseAmount} />
-					<button
-						onClick={() => {
-							const name = expenseName.current?.value;
-							const amount = expenseAmount.current?.value;
-							if (name && amount) {
-								expenseName.current.value = '';
-								expenseAmount.current.value = '';
-								setBudget(prevBudget =>
-									addBudgetItem(prevBudget, { name, amount: Number(amount) })
-								);
-							}
-						}}
-					>
-						Submit
-					</button>
-				</div>
-				<div>
-					<h3>Expenses items:</h3>
-					<ul>
-						{budgetItems.map((node, i) => (
-							<li key={`${node.item.name}-${i}`}>
-								{node.item.name}: {node.item.amount}
-							</li>
-						))}
-					</ul>
-				</div>
 			</div>
 		</div>
 	);
