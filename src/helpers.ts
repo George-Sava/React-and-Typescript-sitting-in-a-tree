73dd999afb7f90572513823755a05d6cd0834aaa
George-Sava <sava_george.alex@yahoo.com>
2023-03-25 20:34:09 +0200

    new styles, components and minor bux fix

--- src/helpers.ts
+++ src/helpers.ts
@@ -1,70 +1,37 @@
-import { BudgetItem, BudgetNode, BudgetNodeVisitor } from 'types';
+import { BudgetNode } from 'types';
 
-export function addItem(node: BudgetNode | null | undefined, item: BudgetItem): BudgetNode {
-	if (node === undefined || node === null) {
-		return { item };
-	}
-
-	if (node.item.name === item.name) {
-		return { ...node, item: { ...node.item, amount: node.item.amount + item.amount } };
-	}
-
-	return node;
-}
-
-export function addBudgetItem(node: BudgetNode | null | undefined, item: BudgetItem): BudgetNode {
-	if (node === undefined || node === null) {
-		return addItem(node, item);
-	}
-
-	if (node.item.name === item.name) {
-		return addItem(node, item);
-	}
-
-	if (!node.left) {
-		return {
-			...node,
-			left: addItem(node.left, item)
-		};
-	}
-
-	if (!node.right) {
-		return {
-			...node,
-			right: addItem(node.right, item)
-		};
-	}
-
-	return {
-		...node,
-		left: addBudgetItem(node.left, item),
-		right: addBudgetItem(node.right, item)
-	};
-}
-export function getTotalBudget(node: BudgetNode | null | undefined): number {
+export function getTotalBudget(node: BudgetNode | undefined): number {
 	if (!node) {
 		return 0;
 	}
 
 	const currentAmount = node.item.amount;
-	const leftAmount = getTotalBudget(node.left);
-	const rightAmount = getTotalBudget(node.right);
+	const leftSubtreeAmount = getTotalBudget(node.left);
+	const rightSubtreeAmount = getTotalBudget(node.right);
 
-	return currentAmount + leftAmount + rightAmount;
-}
-export function getBudgetItems(node: BudgetNode | null): BudgetNode[] {
-	return node ? visitNodes(node, node => node) : [];
+	return currentAmount + leftSubtreeAmount + rightSubtreeAmount;
 }
 
-export function visitNodes<T>(
-	node: BudgetNode | null | undefined,
-	visitor: BudgetNodeVisitor<T>
-): T[] {
-	if (node === undefined || node === null) {
-		return [];
+function visitNodes(node: BudgetNode | undefined, callback: (node: BudgetNode) => void) {
+	if (node === undefined) {
+		return;
 	}
-	const left = visitNodes(node.left, visitor);
-	const self = visitor(node);
-	const right = visitNodes(node.right, visitor);
-	return [...left, self, ...right];
+
+	callback(node);
+	visitNodes(node.left, callback);
+	visitNodes(node.right, callback);
+}
+
+export function getBudgetItems(node: BudgetNode | undefined): BudgetNode[] {
+	const nodes: BudgetNode[] = [];
+	visitNodes(node, node => nodes.push(node));
+	return nodes;
+}
+
+export function getDepth(node: BudgetNode | undefined): number {
+	if (!node) {
+		return 0;
+	}
+
+	return Math.max(getDepth(node.left), getDepth(node.right)) + 1;
 }
