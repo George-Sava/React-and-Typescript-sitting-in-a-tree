eb60ea677fdbc2c4a9e0a5bff40f3a77bf642225
George-Sava <sava_george.alex@yahoo.com>
2023-03-26 10:30:54 +0300

    starting-point v2

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,11 +1,9 @@
 import './App.css';
-interface Props {
-	text?: string;
-}
 
-export function App({ text }: Props) {
-	if (text) {
-		return <div>{text}</div>;
-	}
-	return <div>Hello</div>;
+export function App() {
+	return (
+		<div className="flex-container">
+			<h1>Budget App</h1>
+		</div>
+	);
 }
